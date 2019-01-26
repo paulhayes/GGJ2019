9bdf89bffccbf4f84cfd9b0a3159839a39b7f459
Owen Daughtery <OwenJ.Daughtery@gmail.com>
2019-01-26 14:08:04 +0000

    Working on game structure :construction:

--- Assets/Main/Scripts/Gap.cs
+++ Assets/Main/Scripts/Gap.cs
@@ -7,11 +7,38 @@ public class Gap : MonoBehaviour
     [SerializeField] Transform start;
     [SerializeField] Transform end;
 
+    [SerializeField] Cinemachine.CinemachineVirtualCamera _camera;
 
     [SerializeField] GapSpace gapSpace;
+
+    [SerializeField] GameObject targetPosition;
 
     GapSpace GetSpace()
     {
         return gapSpace;
     }
+
+    public void Select()
+    {
+        _camera.gameObject.SetActive(true);
+        _camera.LookAt = targetPosition.transform;
+        _camera.Follow = targetPosition.transform;
+    }
+
+    public void Deselect()
+    {
+        _camera.gameObject.SetActive(false);
+    }
+
+    public Vector3 GetNearest()
+    {
+        return Vector3.zero;
+    }
+
+    public Vector3 Move(float delatPos)
+    {
+        return Vector3.zero;
+    }
+
+
 }
