1f39ac4663fb7cb39d06942963a2a710f32c1623
Owen Daughtery <OwenJ.Daughtery@gmail.com>
2019-01-26 13:57:27 +0000

    Main game mechanic structure :construction:

---
+++ Assets/Main/Scripts/Gap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gap : MonoBehaviour
+{
+    [SerializeField] Transform start;
+    [SerializeField] Transform end;
+
+
+    [SerializeField] GapSpace gapSpace;
+
+    GapSpace GetSpace()
+    {
+        return gapSpace;
+    }
+}
---
+++ Assets/Main/Scripts/Views/SearchView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchView : AbstractView
+{
+    public override void Begin()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void End()
+    {
+        throw new System.NotImplementedException();
+    }
+}
