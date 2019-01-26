87c529f3edd1f01b4926674e36b4cc98f531ef5d
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-26 18:59:43 +0000

    :sparkles: Made dialog elements not fade out if they are being replaced by the same text. Removed the sketchy text background and created a faded one.

---
+++ Assets/ObjectCanvas.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCanvasToken
+{
+	public ObjectCanvas Canvas;
+
+	public ObjectCanvasToken(ObjectCanvas canvas)
+	{
+		Canvas = canvas;
+	}
+}
+
+public class ObjectCanvas : MonoBehaviour
+{
+	public static ObjectCanvas Instance;
+
+	public RectTransform Holder;
+
+	private GameObject destroy;
+
+	private void Awake ()
+	{
+		Instance = this;
+	}
+
+	public ObjectCanvasToken Render(GameObject target)
+	{
+		var clone = Instantiate (target);
+
+		clone.transform.SetParent (Holder);
+		clone.transform.localPosition = Vector3.zero;
+
+		destroy = clone;
+
+		return new ObjectCanvasToken (this);
+	}
+
+	public void Clear()
+	{
+		Destroy (destroy);
+	}
+}
