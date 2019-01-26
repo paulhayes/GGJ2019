ea78961d72ebdc71a03b3b19efb636dcd6a7e47d
Evan Griffiths <evangriffiths@hotmail.co.uk>
2019-01-26 12:43:50 +0000

    :construction: Added crease colliders to initial scene and created PlayerController and PlayerInput scripts

---
+++ Assets/Evan/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 armMinMaxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
