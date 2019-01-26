c920a82da3a5ba1739ef98935a40ba58c8cac7bb
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-26 16:43:29 +0000

    :sparkles: Finished off and implemented the dialog system in Scene 1

--- Assets/Scripts/Subtitles/Dialog.cs
+++ Assets/Scripts/Subtitles/Dialog.cs
@@ -15,11 +15,13 @@ public class Dialog : ScriptableObject
 			Aside
 		}
 
+		[TextArea(3, 10)]
 		public string Text;
 		public Options Area;
 	}
-	
+
 	public DisplayArea[] Displays = new DisplayArea[1];
+	public float Delay;
 	public float Duration = 2.0f;
 	public AudioClip PlaySound;
 }
