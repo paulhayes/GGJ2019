113f0e8dd9eabea0649ffd96446b323f3fc5f1e2
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-27 07:17:33 +0000

    :art: Added glowing indicators on the lockbox.

---
+++ Assets/Main/Scripts/Lockbox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lockbox : MonoBehaviour
+{
+    [SerializeField] GameObject gapIndicator;
+	[SerializeField] GameObject dangerIndicator;
+	[SerializeField] Animator openAnimator;
+	[SerializeField] Dialog TryOpenDialog;
+
+	public bool CanOpen;
+
+	[SerializeField] Cinemachine.CinemachineVirtualCamera _camera;
+
+	private void OnMouseEnter ()
+	{
+		if (CanOpen)
+			gapIndicator.SetActive (true);
+		else
+			dangerIndicator.SetActive (true);
+	}
+
+	private void OnMouseExit ()
+	{
+		if (CanOpen)
+			gapIndicator.SetActive (false);
+		else
+			dangerIndicator.SetActive (false);
+	}
+
+	private void OnMouseDown ()
+	{
+		Debug.Log ("Clicked");
+		if(CanOpen)
+		{
+			openAnimator.SetTrigger ("Play");
+		}
+		else
+		{
+			if (TryOpenDialog != null)
+				DialogManager.Instance.Play (TryOpenDialog);
+		}
+	}
+}
