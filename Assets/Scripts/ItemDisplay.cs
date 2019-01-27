d795b9f773f9fbc9e0dd6e55f6f2113381004cc1
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-27 03:52:45 +0000

    :sparkles: Added the progress tracking lockbox on the sofa with a progress bar and a collection of misc items lying around it.

---
+++ Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ItemDisplay : MonoBehaviour
+{
+	public Item Identifier;
+	public AnimationCurve InCurve;
+	public float InDuration = 1.0f;
+	public GameObject Rendering;
+
+	private void Start ()
+	{
+		Rendering.SetActive (false);
+	}
+
+	public void Play ()
+	{
+		Rendering.SetActive (true);
+		StartCoroutine (PlayCoroutine ());
+	}
+
+	private IEnumerator PlayCoroutine()
+	{
+		foreach(var time in new TimedLoop(InDuration))
+		{
+			Rendering.transform.localScale = Vector3.one * InCurve.Evaluate (time);
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Scripts/ObjectCanvas.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCanvas : MonoBehaviour
+{
+	public static ObjectCanvas Instance;
+
+	public Transform Holder;
+
+	public GameObject LastTarget;
+	private GameObject destroy;
+	
+	public CurveInterpolator EntryBounce;
+	
+	private void Awake ()
+	{
+		Instance = this;
+	}
+
+	private void Update ()
+	{
+		EntryBounce.Update (Time.deltaTime);
+
+		Holder.localScale = Vector3.one * EntryBounce.Value;
+	}
+
+	public void Render(GameObject target)
+	{
+		if(destroy != null)
+			Destroy (destroy);
+
+		LastTarget = target;
+
+		var clone = Instantiate (target);
+
+		clone.transform.SetParent (Holder);
+		clone.transform.localPosition = Vector3.zero;
+		clone.transform.localScale = Vector3.one;
+		clone.transform.localRotation = Quaternion.identity;
+
+		destroy = clone;
+		EntryBounce.TargetValue = 1.0f;
+	}
+
+	public void Clear()
+	{
+		EntryBounce.TargetValue = 0.0f;
+		// Destroy (destroy);
+	}
+}
